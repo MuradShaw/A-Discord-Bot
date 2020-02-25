27346fc46957aa9577d24d3fac1734ec519d0a89
MuradShaw <60895889+MuradShaw@users.noreply.github.com>
2020-02-24 22:10:18 -0500

    Uploaded locally

---
+++ index.js
@@ -0,0 +1,21 @@
+//Getting things ready
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+//Bot running
+client.once('ready', () => {
+    console.log('Ready!')
+})
+
+//Message was sent somewhere
+client.on('message', message => {
+
+    //Someone is trying to run a command
+    if(message.content.startsWith(`${prefix}`))
+    {
+
+    }
+})
+
+client.login(token); //Grab token and run
