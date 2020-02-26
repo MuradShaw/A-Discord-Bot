51d27ea8f30410b1a0d4c9ec13a4fdcd06913e2d
MuradShaw <60895889+MuradShaw@users.noreply.github.com>
2020-02-26 11:03:54 -0500

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -1,6 +1,7 @@
 //For creating and interacting with MySql DB
 
 var mysql = require('mysql');
+var index = require('./index.js')
 const { Host, User, Password, Database } = require('./settings.json');
 
 //Create connection
@@ -11,16 +12,29 @@ var con = mysql.createConnection({
   database: Database
 });
 
-//Did we attempt a connection?
+//Attempt a connection
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
 
 //Start authentication
-function prepareAuthentication()
+function prepareAuthentication(var discordID)
 {
-  var sql = "INSERT into adb_auth (did, code) VALUES ?";
+  var authCode = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15); //Generate code
+  var sql = "INSERT into adb_auth (did, code) VALUES ?"; //Sql command
+  var data = [discordID, authCode]; //Sql param
+  
+  var errored = false;
+  
+  //Do the query
+  con.query(sql, [data], function (err, result) {
+    errored = err;
+  });
+  
+  return new Promise(function(resolve, reject) {
+          resolve(errored); //Make sure nothing goes wrong and ruins the dinner
+  });
 }
 
 module.exports = {
