5b6121f4857587f6d04d6256c2ae5167cbc1a315
MuradShaw <60895889+MuradShaw@users.noreply.github.com>
2020-02-28 13:10:03 -0500

    Update textembs.js

--- textembs.js
+++ textembs.js
@@ -1,1 +1,21 @@
 const Discord = require('discord.js');
+const { currencyName } = require('./config.json');
+
+const createInfoEmbed = (messageAuthorData) => {
+	const infoEmbed = new Discord.RichEmbed()
+					.setColor('#0099ff')
+					.setTitle(message.author.username)
+					//.setURL(message.author.fetchProfile)
+					//.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+					.setDescription(message.author.tag)
+					.setThumbnail(message.author.avatarURL)
+					.addBlankField()
+					.addField(currencyName, amount, true)
+					.addField('EXP', '7', true)
+					.addField('Equipped Weapon', 'Toast Sword', true)
+					.setImage(message.author.avatarURL)
+					.setTimestamp()
+					.setFooter('A Discord Bot', 'https://i.imgur.com/wSTFkRM.png');
+}
+
+module.exports = createInfoEmbed
