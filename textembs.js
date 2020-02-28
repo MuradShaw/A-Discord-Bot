642e3070cb57a39e55a6956883b76b6486081154
MuradShaw <60895889+MuradShaw@users.noreply.github.com>
2020-02-28 13:18:44 -0500

    Update textembs.js

--- textembs.js
+++ textembs.js
@@ -1,21 +1,23 @@
 const Discord = require('discord.js');
 const { currencyName } = require('./config.json');
 
-const createInfoEmbed = (messageAuthorData) => {
+const createInfoEmbed = (message) => {
 	const infoEmbed = new Discord.RichEmbed()
-					.setColor('#0099ff')
-					.setTitle(message.author.username)
-					//.setURL(message.author.fetchProfile)
-					//.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-					.setDescription(message.author.tag)
-					.setThumbnail(message.author.avatarURL)
-					.addBlankField()
-					.addField(currencyName, amount, true)
-					.addField('EXP', '7', true)
-					.addField('Equipped Weapon', 'Toast Sword', true)
-					.setImage(message.author.avatarURL)
-					.setTimestamp()
-					.setFooter('A Discord Bot', 'https://i.imgur.com/wSTFkRM.png');
+		.setColor('#0099ff')
+		.setTitle(message.author.username)
+		//.setURL(message.author.fetchProfile)
+		//.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+		.setDescription(message.author.tag)
+		.setThumbnail(message.author.avatarURL)
+		.addBlankField()
+		.addField(currencyName, amount, true)
+		.addField('EXP', '7', true)
+		.addField('Equipped Weapon', 'Toast Sword', true)
+		.setImage(message.author.avatarURL)
+		.setTimestamp()
+		.setFooter('A Discord Bot', 'https://i.imgur.com/wSTFkRM.png');
+	
+	return infoEmbed;
 }
 
 module.exports = createInfoEmbed
